5db25c82d9bd50858a4866d2547001277e0002e2
K Adam White <kadamwhite@users.noreply.github.com>
2020-08-19 11:15:44 -0400

    Remove Altis-specific code

--- inc/namespace.php
+++ inc/namespace.php
@@ -7,32 +7,10 @@
 
 namespace Altis\Local_Server;
 
-use Altis;
-
 /**
  * Configure environment for local server.
  */
 function bootstrap() {
-	add_filter( 'admin_menu', __NAMESPACE__ . '\\tools_submenus' );
-
-	$config = Altis\get_config()['modules']['local-server'];
-
-	if ( $config['s3'] ) {
-		define( 'S3_UPLOADS_BUCKET', getenv( 'S3_UPLOADS_BUCKET' ) );
-		define( 'S3_UPLOADS_REGION', getenv( 'S3_UPLOADS_REGION' ) );
-		define( 'S3_UPLOADS_KEY', getenv( 'S3_UPLOADS_KEY' ) );
-		define( 'S3_UPLOADS_SECRET', getenv( 'S3_UPLOADS_SECRET' ) );
-		define( 'S3_UPLOADS_ENDPOINT', getenv( 'S3_UPLOADS_ENDPOINT' ) );
-		define( 'S3_UPLOADS_BUCKET_URL', getenv( 'S3_UPLOADS_BUCKET_URL' ) );
-
-		add_filter( 's3_uploads_s3_client_params', function ( $params ) {
-			if ( defined( 'S3_UPLOADS_ENDPOINT' ) && S3_UPLOADS_ENDPOINT ) {
-				$params['endpoint'] = S3_UPLOADS_ENDPOINT;
-			}
-			return $params;
-		}, 5, 1 );
-	}
-
 	if ( empty( $_SERVER['HTTP_HOST'] ) ) {
 		$_SERVER['HTTP_HOST'] = getenv( 'COMPOSE_PROJECT_NAME' ) . '.altis.dev';
 	}
@@ -55,33 +33,6 @@ function bootstrap() {
 		'port' => getenv( 'REDIS_PORT' ),
 	];
 
-	if ( $config['tachyon'] ) {
-		define( 'TACHYON_URL', getenv( 'TACHYON_URL' ) );
-
-		/**
-		 * In local-server, the tachyon hostname resolves to what is deemed a local url.
-		 * This makes requests to tachyon from WordPress disallowed. We want to
-		 * specifically allow that host.
-		 */
-		add_filter( 'http_request_host_is_external', function ( bool $is_external, string $host ) : bool {
-			if ( $is_external ) {
-				return $is_external;
-			}
-
-			// @codingStandardsIgnoreLine
-			return parse_url( TACHYON_URL, PHP_URL_HOST ) === $host;
-		}, 10, 2 );
-	}
-
-	if ( $config['analytics'] ) {
-		define( 'ALTIS_ANALYTICS_PINPOINT_ID', '12345678901234567890123456' );
-		define( 'ALTIS_ANALYTICS_PINPOINT_REGION', 'us-east-1' );
-		define( 'ALTIS_ANALYTICS_COGNITO_ID', 'us-east-1:f6f6f6-fafa-f5f5-8f8f-1234567890' );
-		define( 'ALTIS_ANALYTICS_COGNITO_REGION', 'us-east-1' );
-		define( 'ALTIS_ANALYTICS_PINPOINT_ENDPOINT', getenv( 'ALTIS_ANALYTICS_PINPOINT_ENDPOINT' ) );
-		define( 'ALTIS_ANALYTICS_COGNITO_ENDPOINT', getenv( 'ALTIS_ANALYTICS_COGNITO_ENDPOINT' ) );
-	}
-
 	add_filter( 'qm/output/file_path_map', __NAMESPACE__ . '\\set_file_path_map', 1 );
 
 	// Filter ES package IDs for local.
@@ -101,30 +52,6 @@ function set_file_path_map( array $map ) : array {
 	}
 	$map['/usr/src/app'] = rtrim( getenv( 'HOST_PATH' ), DIRECTORY_SEPARATOR );
 	return $map;
-}
-
-/**
- * Add new submenus to Tools admin menu.
- */
-function tools_submenus() {
-	$links = [
-		[
-			'label' => 'Kibana',
-			'url' => network_site_url( '/kibana' ),
-		],
-		[
-			'label' => 'MailHog',
-			'url' => network_site_url( '/mailhog' ),
-		],
-		[
-			'label' => 'S3 Browser',
-			'url' => S3_UPLOADS_BUCKET_URL . '/minio',
-		],
-	];
-
-	foreach ( $links as $link ) {
-		add_management_page( $link['label'], $link['label'], 'manage_options', $link['url'] );
-	}
 }
 
 /**
